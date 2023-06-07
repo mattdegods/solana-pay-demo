fa80f22dd805c0e9fc8a7323b66e5e9d1307c94f
mattdegods <matthewcpfeifer@gmail.com>
2023-06-07 09:57:36 -0400

    Fix: update branding

--- components/SiteHeading.tsx
+++ components/SiteHeading.tsx
@@ -2,10 +2,7 @@ import { PropsWithChildren } from 'react'
 
 export default function SiteHeading({ children }: PropsWithChildren<{}>) {
   return (
-    <h1
-      className="mt-8 mb-4 self-center bg-gradient-to-r from-fuchsia-600 
-      to-pink-600 bg-clip-text text-6xl font-extrabold text-transparent"
-    >
+    <h1 className="self-center text-5xl font-extrabold text-black">
       {children}
     </h1>
   )
--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -3,8 +3,11 @@ import SiteHeading from '../../components/SiteHeading'
 
 export default function ShopPage() {
   return (
-    <div className="m-auto flex max-w-4xl flex-col items-stretch gap-8 pt-24">
-      <SiteHeading>☕️ Upstate Coffee Collective ☕️</SiteHeading>
+    <div className="m-auto flex max-w-4xl flex-col items-stretch gap-8">
+      <div className="mx-auto w-fit">
+        <img src="/upcc.jpg" width={100} height={100} alt="upcc icon" />
+      </div>
+      <SiteHeading>Upstate Coffee Collective</SiteHeading>
       <Products submitTarget="/shop/checkout" enabled={true} />{' '}
     </div>
   )
